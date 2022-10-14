c9f37f496b875b3664079b39710eabfe09772cc9
Danyyl Dolynsky <dolynskydanyyl@gmail.com>
2022-10-14 09:53:23 +0300

    complete xml and json format

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 from flask import Flask
 from flask_restful import Api
 from handlers.api import DriversList
+from flasgger import Swagger
+from flask import make_response
+import json
+from simplexml import dumps
+
 
 
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True
 
+    create_swagger(app)
     create_api(app)
 
     return app
 
 
+def create_swagger(app):
+    app.config["SWAGGER"] = {
+        'title': 'Drivers List',
+        'doc_dir': './examples/docs/'
+    }
+    swagger = Swagger(app)
+    return swagger
+
+
 def create_api(app):
     api = Api(app)
+
+    @api.representation('application/json')
+    def output_json(data, code, headers=None):
+        resp = make_response(json.dumps({data}), code)
+        resp.headers.extend(headers or {})
+        return resp
+
+    @api.representation('application/xml')
+    def output_xml(data, code, headers=None):
+        resp = make_response(dumps({'response': data}), code)
+        resp.headers.extend(headers or {})
+        return resp
+
     api.add_resource(DriversList, '/api/v1/report/')
     api.init_app(app)
     return api
--- handlers/api.py
+++ handlers/api.py
@@ -1,30 +1,36 @@
-from flask import jsonify
-from flask_restful import Resource, abort, reqparse, request
+from flask import jsonify, make_response
+from flask_restful import Resource, request
 from report_of_monaco_2018_racing_dan import report
-from dict2xml import dict2xml
+from dicttoxml import dicttoxml
 
 
 class DriversList(Resource):
     DRIVERS = {}
 
     for driver in report.create_drivers():
-        DRIVERS[f"driver {driver.number}"] = {
+        DRIVERS[f"driver{driver.number}"] = {
             driver.code:
                 {
-                    "number": driver.number,
-                    "name": driver.name,
-                    "company": driver.company,
-                    "resul": driver.result
+                    "number":
+                    driver.number,
+                    "name":
+                    driver.name,
+                    "company":
+                    driver.company,
+                    "resul":
+                    driver.result
                 }
         }
 
-
-
     def get(self):
-        if request.args.get('format') == 'json':
-            resp = jsonify(DriversList.DRIVERS)
-            return resp, 200
+        if request.args.get('format') == 'xml':
+            d = dicttoxml(DriversList.DRIVERS)
+            response = make_response(d)
+            response.headers['content-type'] = 'application/xml'
 
-        elif request.args.get('format') == 'xml':
-            xml = dict2xml(DriversList.DRIVERS)
-            return xml, 200
+        elif request.args.get('format') == 'json':
+            d = jsonify(DriversList.DRIVERS)
+            response = make_response(d)
+            response.headers["Content-Type"] = "application/json"
+
+        return response
